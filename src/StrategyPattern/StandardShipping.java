70dc19e51b7763e5d498875f06359a09b1f569f0
Göktuğ Berke Güngören <132768516+Goktugberke@users.noreply.github.com>
2024-12-02 14:51:45 +0300

    other classes added

---
+++ src/StrategyPattern/StandardShipping.java
@@ -0,0 +1,13 @@
+package StrategyPattern;
+
+public class StandardShipping implements CargoStrategy {
+    @Override
+    public double calculateShipping(Order order) {
+        double baseRate = 5.0; // Temel ücret
+        double weightFactor = 0.1; // Ağırlık başına ücret
+        double distanceFactor = 0.05; // Mesafe başına ücret
+
+        return baseRate + (order.getWeight() * weightFactor) + (order.getDistance() * distanceFactor);
+    }
+}
+
